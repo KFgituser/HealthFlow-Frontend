642c4ec07eebec86357fab65818e955cb7846854
Eric Hao <hkf1239948@163.com>
2025-08-05 21:48:44 +0100

    Update description

--- src/pages/Appointment.js
+++ src/pages/Appointment.js
@@ -217,7 +217,7 @@ import Breadcrumb from '../components/Breadcrumb';
                             src={doctor.imageUrl || '/images/default-doctor.png'}
                             alt={doctor.name}
                             className="rounded-circle"
-                            style={{ width: '270px', height: '27px', objectFit: 'cover' }}
+                            style={{ width: '270px', height: '270px', objectFit: 'cover' }}
                             />
                             </div>
                         )}
